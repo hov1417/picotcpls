c5f4bbfc85c1afbc34cae033537b8ceb0655da08
Florentin Rochet <florentin.rochet@gmail.com>
2020-12-22 18:50:09 +0100

    add a static topology with globally rotable v4 and v6

--- t/ipmininet/simple_migration2.py
+++ t/ipmininet/simple_migration2.py
@@ -3,6 +3,7 @@ from ipmininet.ipnet import IPNet
 from ipmininet.cli import IPCLI
 from ipmininet.link import TCIntf
 from mininet.log import lg
+from ipmininet.router.config import OSPF, RouterConfig
 
 class MyTopology(IPTopo):
 
@@ -21,15 +22,12 @@ class MyTopology(IPTopo):
         r4 = self.addRouter("r4", use_v4=False)
         c = self.addHost("c")
         s = self.addHost("s")
-
-
-        self.addLink(c, r1)
-        self.addLink(r1, r2, delay="10ms", bw=30)
-        self.addLink(r2, r3, delay="10ms", bw=30)
-        self.addLink(r1, r4, delay="20ms", bw=30)
-        self.addLink(r4, r3, delay="20ms", bw=30)
-        self.addLink(r3, s)
-
+        r1r2 = self.addLink(r1, r2, delay="10ms", bw=30)
+        r2r3 = self.addLink(r2, r3, delay="10ms", bw=30)
+        r1r4 = self.addLink(r1, r4, delay="20ms", bw=30)
+        r4r3 = self.addLink(r4, r3, delay="20ms", bw=30)
+        
+        self.addLinks((c, r1, {'params1': {'ip':("2001:12::1/64", "134.104.0.1/24")}, 'params2': {'ip':("2001:13::1/1", "134.104.1.1/24")}}), (r3, s, {'params1':{'ip': ("2001:cafe::1/64", "134.205.0.1/24")}, 'params2':{'ip': ("2001:caf::1/64","134.205.1.1/24")}}))
 
         super().build(*args, **kwargs)
 
@@ -37,6 +35,19 @@ lg.setLogLevel("info")
 net = IPNet(topo=MyTopology(), intf=TCIntf)
 try:
     net.start()
+    #net["c"].cmd("ip -4 route add 134.104.1.1 dev c-eth0")
+    #net["c"].cmd("ip -4 route add 134.205.1.1 dev c-eth0")
+    #net["r1"].cmd("ip -4 route add 134.104.0.1 dev r1-eth2")
+    #net["c"].cmd("ip -6 route add 2001:13::1 dev c-eth0")
+    #net["r1"].cmd("ip -6 route add 2001:12::1 dev r1-eth2")
+    #net["r1"].cmd("ip -4 route add 134.205.1.1 dev r1-eth0")
+    #net["r1"].cmd("ip -6 route add 2001:caf::1 dev r1-eth1")
+    #net["r2"].cmd("ip -4 route add 134.104.0.1 dev r2-eth0")
+    #net["r2"].cmd("ip -4 route add 134.205.1.1 dev r2-eth1")
+    #net["s"].cmd("ip -4 route add 134.104.0.1 dev s-eth0")
+    #net["r3"].cmd("ip -4 route add 134.204.1.1 dev r3-eth2")
+    #net["r3"].cmd("ip -4 route add 134.104.0.1 dev r3-eth0")
     IPCLI(net)
 finally:
     net.stop()
+
---
+++ t/ipmininet/static_topo.py
@@ -0,0 +1,85 @@
+from ipmininet.iptopo import IPTopo
+from ipmininet.ipnet import IPNet
+from ipmininet.cli import IPCLI
+from ipmininet.link import TCIntf
+from mininet.log import lg
+from ipmininet.router.config import RouterConfig, STATIC, StaticRoute
+
+class MyTopology(IPTopo):
+
+    def build(self, *args, **kwargs):
+        """
+                 -----r2-----
+                /             \ 
+       c --- r1                r3 --- s
+                \             /
+                 -----r4------
+        """
+
+        r1 = self.addRouter("r1", config=RouterConfig,\
+                            lo_addresses=["2042:1::1/64", "10.50.0.1/24"])
+        r2 = self.addRouter("r2", config=RouterConfig,
+                            lo_addresses=["10.51.0.1/24"])
+        r3 = self.addRouter("r3", config=RouterConfig,\
+                            lo_addresses=["2042:2::1/64", "10.52.0.1/24"])
+        r4 = self.addRouter("r4", config=RouterConfig,
+                            lo_addresses=["2042:3::1/64"])
+        c = self.addHost("c")
+        s = self.addHost("s")
+        
+        cr1 = self.addLink(c, r1)
+        cr1[c].addParams(ip=("2001:abe::1/64", "130.104.205.174/24"))
+        cr1[r1].addParams(ip=("2001:abe::2/64", "130.104.205.1/24"))
+
+        r1r2 = self.addLink(r1, r2, delay="10ms", bw=30)
+        r1r2[r1].addParams(ip=("10.1.0.1/24"))
+        r1r2[r2].addParams(ip=("10.1.0.2/24"))
+
+        r2r3 = self.addLink(r2, r3, delay="10ms", bw=30)
+        r2r3[r2].addParams(ip=("10.2.0.1/24"))
+        r2r3[r3].addParams(ip=("10.2.0.2/24"))
+
+        r3s = self.addLink(r3, s)
+        r3s[s].addParams(ip=("2001:cafe::1/64", "50.50.50.5/24"))
+        r3s[r3].addParams(ip=("2001:cafe::a/64", "50.50.50.1/24"))
+
+        r1r4 = self.addLink(r1, r4, delay="20ms", bw=30)
+        r1r4[r1].addParams(ip=("2001:1a::1/64"))
+        r1r4[r4].addParams(ip=("2001:1a::a/64"))
+
+        r4r3 = self.addLink(r4, r3, delay="20ms", bw=30)
+        r4r3[r4].addParams(ip=("2001:2b::2/64"))
+        r4r3[r3].addParams(ip=("2001:2b::b/64"))
+
+        r1.addDaemon(STATIC, static_routes=[StaticRoute("50.50.50.0/24",\
+                                                        "10.1.0.2"),\
+                                            StaticRoute("10.2.0.0/24",\
+                                                        "10.1.0.2"),\
+                                            StaticRoute("2001:cafe::/64",\
+                                                        "2001:1a::a")])
+        r2.addDaemon(STATIC, static_routes=[StaticRoute("50.50.50.0/24",\
+                                                        "10.2.0.2"),\
+                                            StaticRoute("130.104.205.0/24",\
+                                                        "10.1.0.1")])
+        r3.addDaemon(STATIC, static_routes=[StaticRoute("130.104.205.0/24",\
+                                                        "10.2.0.1"),\
+                                            StaticRoute("10.1.0.0/24",\
+                                                        "10.2.0.1"),\
+                                            StaticRoute("2001:abe::/64",\
+                                                        "2001:2b::2")])
+        
+        r4.addDaemon(STATIC, static_routes=[StaticRoute("2001:abe::/64",\
+                                                      "2001:1a::1"),\
+                                          StaticRoute("2001:cafe::/64",\
+                                                      "2001:2b::b")])
+
+        super().build(*args, **kwargs)
+
+lg.setLogLevel("info")
+net = IPNet(topo=MyTopology(), intf=TCIntf, allocate_IPs=False)
+try:
+    net.start()
+    IPCLI(net)
+finally:
+    net.stop()
+
