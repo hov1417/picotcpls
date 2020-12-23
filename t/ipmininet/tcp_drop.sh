bf4bbb107406f3523707ce4df84d9dee7050d7d4
Florentin Rochet <florentin.rochet@gmail.com>
2020-12-23 16:30:54 +0100

    up

--- t/ipmininet/tcp_drop.sh
+++ t/ipmininet/tcp_drop.sh
@@ -4,20 +4,21 @@ IP_SRC_1=10.0.0.1
 IP_SRC_2=10.0.1.1
 IP_DEST_1=10.1.0.1
 IP_DEST_2=10.1.1.1
-PATH=0
+IPPATH=0
+
 sleep 5
 printf "Starting to backhole traffic\n"
 while true;
 do
-  if [ $PATH -eq 0 ]
+  if [ $IPPATH -eq 0 ]
   then
-    print "Blackholing Path 0\n"
+    printf "Blackholing Path 0\n"
     iptables -A FORWARD -p tcp -s $IP_SRC_1 -d $IP_DEST_1 -j DROP
-    PATH=1
+    IPPATH=1
   else
-    print "Blackholing Path 1\n"
+    printf "Blackholing Path 1\n"
     iptables -A FORWARD -p tcp -s $IP_SRC_2 -d $IP_DEST_2 -j DROP
-    PATH=0
+    IPPATH=0
   fi
   sleep $TIME
   iptables -F
